583f88e1bb3438d5e158d6c6a62d8399c574cafa
Pdq1227 <80385671+Pdq1227@users.noreply.github.com>
2021-12-18 02:41:49 +0700

    Update

--- app.py
+++ app.py
@@ -24,23 +24,18 @@ from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity, linear_kernel
 
 
-
-
-# @app.route("/")
-# def main():
-#     return "Welcome!"
-    
 @app.route("/", methods=['GET','POST'])
 def track_id():
-    output=None
+    output1 = None
+    output2 = None
     track_name_result = ""
     if request.method == 'POST':
         track_name=request.form['track_name']
         track_id = 0
-        genre1=request.form['genre1']
-        genre2=request.form['genre2']
-        genre3=request.form['genre3']
-        lyric=request.form['lyrics']
+        genre1 = request.form['genre1']
+        genre2 = request.form['genre2']
+        genre3 = request.form['genre3']
+        lyric = request.form['lyrics']
 
         #Load data
         df = pd.read_csv("./valence_arousal_dataset.csv")
@@ -49,9 +44,9 @@ def track_id():
         df["mood_vec"] = df[["valence", "energy"]].values.tolist()
 
         sp = authorization.authorize()
-        track_name_result=search_engine(track_name)
+        track_name_result = search_engine(track_name)
         if len(track_name_result) not in (0,1):
-             track_id = df[df['track_name']==track_name_result]['id'].values[0]
+             track_id = df[df['track_name'] == track_name_result]['id'].values[0]
 
         # In order to compute distances between two tracks, we need to transform the seperate valence and energy columns to a mood-vector column. 
         # This can be done by using df.apply() alongside a lambda function
@@ -63,62 +58,79 @@ def track_id():
         #3.Sort the reference track from lowest to highest distance.
         #4.Return the n most similar tracks
 
-        def recommend(track_id, ref_df, sp, n_recs = 5):
-    
+        def recommend(track_id, ref_df, sp, n_recs=5):
             # Crawl valence and arousal of given track from spotify api
             track_features = sp.track_audio_features(track_id)
             track_moodvec = np.array([track_features.valence, track_features.energy])
-            print(f"mood_vec for {track_id}: {track_moodvec}")
-    
+            # print(f"mood_vec for {track_id}: {track_moodvec}")
             # Compute distances to all reference tracks
             ref_df["distances"] = ref_df["mood_vec"].apply(lambda x: norm(track_moodvec-np.array(x)))
             # Sort distances from lowest to highest
             ref_df_sorted = ref_df.sort_values(by = "distances", ascending = True)
             # If the input track is in the reference set, it will have a distance of 0, but should not be recommended
             ref_df_sorted = ref_df_sorted[ref_df_sorted["id"] != track_id]
-    
             # Return n recommendations
             return ref_df_sorted.iloc[:n_recs]
         if track_id in df['id'].unique():
             rec = recommend(track_id=track_id, ref_df= df, sp= sp, n_recs=20)
         else:
-            rec = pd.DataFrame(columns = ['id', 'genre', 'track_name','artist_name','valence','energy','lyric','mood'])
-
-        print(rec)
+            rec = pd.DataFrame(columns=['id', 'genre', 'track_name','artist_name','valence','energy','lyric','mood'])
+        # print(rec)
         list_music = rec.copy()
         if genre1 in df['genre'].unique() or genre2 in df['genre'].unique() or genre3 in df['genre'].unique():
             list_music = list_music[list_music['genre'].isin([genre1, genre2, genre3])]
         if lyric == 'Y' or lyric == 'N':
             list_music = list_music[list_music['lyric'].isin([lyric])]
+        output1 = makeObject(list_music)
+    recom_list = pd.read_csv('./10000.txt', sep='\t')
+    grouped_list = recom_list.groupby('song_ids').sum().reset_index()
+    grouped_list.sort_values(by='listentime', ascending=False, inplace=True)
+    grouped_list = grouped_list.iloc[0:10]
+    linked_list = pd.read_csv('./song_data.csv')
+    songs = []
+    artists = []
+    for i in range(len(grouped_list)):
+        idx = linked_list[linked_list['song_id'] == grouped_list.iloc[i]['song_ids']]['title'].index[0]
+        song = linked_list.iloc[idx]['title']
+        artist = linked_list.iloc[idx]['artist_name']
+        songs.append(song)
+        artists.append(artist)
+    grouped_list['song'] = songs
+    grouped_list['artist'] = artists
+    grouped_list.drop(['song_ids'], axis=1, inplace=True)
+    output2 = []
+    rank = 1
+    for index, row in grouped_list.iterrows():
+        item = {
+            'artist_name': row['artist'],
+            'track_name': row['song'],
+            'listentime': row['listentime'],
+            'rank': rank
+        }
+        rank += 1
+        output2.append(item)
+    print(output2)
+    return render_template('abc.html', output1=output1, track_name_result=track_name_result, recom=output2)
 
-        output= makeObject(list_music)
 
-    return render_template('abc.html',output=output, track_name_result=track_name_result)
+def recommend():
+    data = pd.read_csv('./10000.txt')
 
-@app.route("/popularity_recomendation", methods=['GET','POST'])
-def popularity_recomendation():
-    output=None
-    if request.method == 'POST':
 
-        rec = recommend(track_id=track_id, ref_df= df, sp= sp, n_recs=5)
-        output= makeObject(rec)
-
-    return render_template('abcde.html',output=output)
 
 def makeObject(df):
-    
-    objectList=[]
+    objectList = []
     for index, row in df.iterrows():
-        item={
-            'id':row['id'],
-            'artist_name':row['artist_name'],
-            'track_name':row['track_name'],
-            'genre':row['genre'],
-            'lyric':row['lyric']
+        item = {
+            'id': row['id'],
+            'artist_name': row['artist_name'],
+            'track_name': row['track_name'],
+            'genre': row['genre'],
+            'lyric': row['lyric']
         }
-        
         objectList.append(item)
     return objectList
+
 
 if __name__ == "__main__":
     app.run(debug=True)
